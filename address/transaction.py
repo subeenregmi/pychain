d71dd826762d28f51776aee2f0e3d9326790b26d
subweeb <72048704+subeenregmi@users.noreply.github.com>
2022-03-09 22:41:03 +0000

    started working on transactions, need to decide how to format a complete tx

--- address/transaction.py
+++ address/transaction.py
@@ -6,6 +6,33 @@ a transaction should include the following:
 1) To address
 2) From address
 3) Amount
-4)
+4) Validation from to address
+5) Any extra data that is needed
+
+We should start by making a function to do so 
+
+then with every transaction we need to create a function to do so and then create a icon 
 
 '''
+
+
+'''class Transaction():
+
+    def __init__(self, _toAddress, _fromAddress, _amount, _validation):
+        self.toAddress = str(_toAddress)
+        self.fromAddress = str(_fromAddress)
+        self.amount = int(_amount)
+        self.validation = bool(_validation)
+
+    def verifySpenderIsOwner(_fromAdress, privKey):
+        
+        verifiedOwner(_fromAdress, privKey)
+'''
+
+'''toAddress = ""
+fromAddress = ""
+amount = ""
+validation = False
+metaData = ""'''
+
+def createTX(toAddress, fromAddress, amount, metadata):
