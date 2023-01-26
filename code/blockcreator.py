f81b25d1c3b2ba3186edbd5a8ec453ca2c79766c
Subeen Regmi <subeenregmi123@gmail.com>
2023-01-26 17:52:46 +0000

    some more stuff

--- code/blockcreator.py
+++ code/blockcreator.py
@@ -36,12 +36,13 @@ TXS
 
 
 class Block():
-    def __init__(self, height, transactions, difficulty, previousblockhash): 
+    def __init__(self, height=None, transactions=None, difficulty=None, previousblockhash=None): 
         self.height = height
         self.transactions = transactions
         merkle = ""
-        for x in self.transactions:
-            merkle += x
+        if self.transactions != None:
+            for x in self.transactions:
+                merkle += x
         self.merkle = hashlib.sha256(merkle.encode('utf-8')).hexdigest()
         self.difficulty = difficulty
         self.previousblockhash = previousblockhash
@@ -49,8 +50,7 @@ class Block():
         self.blockid = 0
         self.blocktime = 0
         self.blockmined = False
-        self.raw = ""
-        
+        self.raw = ""        
 
     def mine(self):
         timeofmine = int(time.time())
@@ -104,16 +104,41 @@ class Block():
             sizeTx = sizeTx.zfill(10)
             raw = self.blockid + nonce + blocktime + blockheight + self.previousblockhash + difficultlyOfBlock + self.merkle + sizeTx + transactions
             self.raw = raw
+            self.addBlockToChain()
+    
+    def addBlockToChain(self):
+        if self.blockmined == False:
+            print("Cannot add a unmined block.")
+        else: 
+            file = open("blockchain.txt", "a")
+            file.write(self.raw)
+            file.write("\n\n")
+            file.close()
+            print("Success")
+    
+    def createBlockFromRaw(self, raw):
+        self.nonce = raw[0:15]
+        self.blocktime = raw[16:28]
+        print(raw[16:28])
+
+        
+
 
 def main():
 
-    x = Block(1, ["0102ab696a951348d80c9360d0de0733eef12c6cd64e7bbaaf658acee42a61d32d600001002046b7ebfe9b9639f6f88f77709b453f89ce380ae192202f1fd913864a4c3144948732d097e715d15e8ddd312749a97572bc97b6f8bc1692f08e82f90d0882258e00010020c2d28ed3a36ca0a8a3076d4c2dfa54c95383deee8ed16b63720f0561a86894650100000000000001f4002676a92169f38a6b51de7e9345992f2161c9c811a8b57cb2c1f31b8f98211b21af61096bd588ac00000000"], 1105681728405912613142410972512193988879657863534672332727522661322334263, "1b7990b9fd11da0d24a0f539e3ed3407285538737785acc6b7dcb602b0a68492")
-    #y = Block(2, ["0102ab696a951348d80c9360d0de0733eef12c6cd64e7bbaaf658acee42a61d32d600001002046b7ebfe9b9639f6f88f77709b453f89ce380ae192202f1fd913864a4c3144948732d097e715d15e8ddd312749a97572bc97b6f8bc1692f08e82f90d0882258e00010020c2d28ed3a36ca0a8a3076d4c2dfa54c95383deee8ed16b63720f0561a86894650100000000000001f400201b7990b9fd11da0d24a0f539e3ed3407285538737785acc6b7dcb602b0a6849200000000"], 110568172798640591261314241097512146939888796578635346723327275266671322463)
+    x = Block(1, ["0102ab696a951348d80c9360d0de0733eef12c6cd64e7bbaaf658acee42a61d32d600001002046b7ebfe9b9639f6f88f77709b453f89ce380ae192202f1fd913864a4c3144948732d097e715d15e8ddd312749a97572bc97b6f8bc1692f08e82f90d0882258e00010020c2d28ed3a36ca0a8a3076d4c2dfa54c95383deee8ed16b63720f0561a86894650100000000000001f4002676a92169f38a6b51de7e9345992f2161c9c811a8b57cb2c1f31b8f98211b21af61096bd588ac00000000"], 1105681728405912613142410972512193984481879657863353467234327275223423263, "1b7990b9fd11da0d24a0f539e3ed3407285538737785acc6b7dcb602b0a68492")
+    # #y = Block(2, ["0102ab696a951348d80c9360d0de0733eef12c6cd64e7bbaaf658acee42a61d32d600001002046b7ebfe9b9639f6f88f77709b453f89ce380ae192202f1fd913864a4c3144948732d097e715d15e8ddd312749a97572bc97b6f8bc1692f08e82f90d0882258e00010020c2d28ed3a36ca0a8a3076d4c2dfa54c95383deee8ed16b63720f0561a86894650100000000000001f400201b7990b9fd11da0d24a0f539e3ed3407285538737785acc6b7dcb602b0a6849200000000"], 110568172798640591261314241097512146939888796578635346723327275266671322463)
     x.mine()
-    x.validateBlock()
-    print(x.blockid)
+    # x.validateBlock()
+    # print(x.blockid)
+    # print(x.blocktime)
+    # x.addBlockToChain()
+    # print(f"Raw: {x.raw}")
+    x.createBlockFromRaw("00000807c9f084501ce44c3874a9806b26a3a265d435f866da0f0b5c33478e900000000000027adf000167475244200000000011b7990b9fd11da0d24a0f539e3ed3407285538737785acc6b7dcb602b0a68492000000000000000000000000000000000000100533936338156e20b56a363abf4c255cc6b5fbcd6b6cf8902840ec991f1d928fe8d935e967c34a7179de60e7c7b20c4dc02283e089ea9d5c31926f1ab1000000017e0102ab696a951348d80c9360d0de0733eef12c6cd64e7bbaaf658acee42a61d32d600001002046b7ebfe9b9639f6f88f77709b453f89ce380ae192202f1fd913864a4c3144948732d097e715d15e8ddd312749a97572bc97b6f8bc1692f08e82f90d0882258e00010020c2d28ed3a36ca0a8a3076d4c2dfa54c95383deee8ed16b63720f0561a86894650100000000000001f4002676a92169f38a6b51de7e9345992f2161c9c811a8b57cb2c1f31b8f98211b21af61096bd588ac00000000")
     print(x.blocktime)
-    print(f"Raw: {x.raw}")
+
+    v = Block()
+    print(v)
 
     
 
