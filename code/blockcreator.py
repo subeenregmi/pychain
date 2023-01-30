bbf0eece26cdcddb7ae79509958982efb235dfa1
Subeen Regmi <subeenregmi123@gmail.com>
2023-01-30 22:17:27 +0000

    added mining address

--- code/blockcreator.py
+++ code/blockcreator.py
@@ -1,44 +1,24 @@
 import hashlib
 import time
+from address import createAddress
 
 """
-block hash 
-
-Height of block
-TX merkle tree
-Nonce
-Difficulty 
-Previous block hash
-
-transactions - dividers between blocks and transactions
-
-size of block data
-
-size of transaction data 
-
-
-1032312312312asd33924939
-
-1
-
-3290w894384f387432g
-
-000003232sdfas
-
-dmasidoj9032938n4f23
-
-TXS
-
-0102ab696a951348d80c9360d0de0733eef12c6cd64e7bbaaf658acee42a61d32d600001002046b7ebfe9b9639f6f88f77709b453f89ce380ae192202f1fd913864a4c3144948732d097e715d15e8ddd312749a97572bc97b6f8bc1692f08e82f90d0882258e00010020c2d28ed3a36ca0a8a3076d4c2dfa54c95383deee8ed16b63720f0561a86894650100000000000001f4002676a92169f38a6b51de7e9345992f2161c9c811a8b57cb2c1f31b8f98211b21af61096bd588ac00000000
-
+Note to self: This class should only be used in the client-side, this should be a way to convert the raw data of block and to 
+validate and mine blocks. Consensus is not achieved here. Also the mine functions need to have a way to create a coinbase transaction
+to reward the miner.
 """
-
-
 
 class Block():
-    def __init__(self, height=None, transactions=None, difficulty=None, previousblockhash=None): 
+    # Constructor to create a block, the block requires multiple parameters or can be created
+    # The block height - this is checked with the blockchain, to create order or multiple versions of that chain
+    # Transactions - these are transactions included within the block, these are required to be verified by each miner
+    # Difficulty - this is usually set by the blockchain, and will be checked to verify if it is a valid block
+    # Previous Block Hash - this is the previous blocks id/hash
+    def __init__(self, height=None, transactions=None, difficulty=None, previousblockhash=None):
+        # The constructor has no default values, as blocks can be created via the raw format 
         self.height = height
         self.transactions = transactions
+        # The merkle root/hash is the raw transactional data hashed together, this creates a unique hash for those transactions
         merkle = ""
         if self.transactions != None:
             for x in self.transactions:
@@ -46,51 +26,87 @@ class Block():
         self.merkle = hashlib.sha256(merkle.encode('utf-8')).hexdigest()
         self.difficulty = difficulty
         self.previousblockhash = previousblockhash
+        # These values are set to zero, false or empty as currently the black has not been mined
         self.nonce = 0
         self.blockid = 0
         self.blocktime = 0
         self.blockmined = False
-        self.raw = ""        
+        self.raw = ""
+        self.miner = "" 
 
-    def mine(self):
+    # This is the algorithm to mine a block, which consists of finding a hash of the block with an included nonce value which 
+    # is lower than the difficulty, thus being a valid block
+    def mine(self, publicKey):
+
+        #Coinbase Transaction will be added for the reward
+        
+
+        # This calculates the string that needs to be hashed
         timeofmine = int(time.time())
         tobehashed = str(timeofmine) + str(self.height) + self.previousblockhash + str(self.difficulty) + self.merkle
         for x in self.transactions:
+            TxLength = str(hex(len(x))[2:]).zfill(8)
+            tobehashed += TxLength
             tobehashed += x
-            
-        print(f"what is to be hashed = {tobehashed}\n")
 
+        minerAddress = createAddress(publicKey)
+        tobehashed += minerAddress
+        
+        # This checks if the hash of the block turns out to be less than difficulty
         while True:
+            # We calculate random hashes by incrimenting a nonce value
             self.nonce += 1
             tobehashed2 = str(self.nonce) + tobehashed
-            #print(f"Hashing ... {tobehashed2}")
-            self.blockid = hashlib.sha256(tobehashed2.encode('utf-8')).hexdigest()
-            #self.blockid = int(self.blockid, 16)
-            print(f"Trying Nonce Value = {self.nonce}, hash is {self.blockid}")
+            tobehashed2 = hashlib.sha256(tobehashed2.encode('utf-8')).hexdigest()
+            print(f"Trying Nonce Value = {self.nonce}, hash is {tobehashed2}")
             
-            if int(self.blockid, 16) < self.difficulty:
-                #self.blockid = hex(self.blockid)[2:]
+            # If the hash is lower than the difficulty, it calculates the raw and the blocktime information
+            if int(tobehashed2, 16) < self.difficulty:
+                self.blockid = tobehashed2
                 self.blocktime = timeofmine
                 print(f"Valid Block Found! NONCE = {self.nonce}, blockid is {self.blockid} at {timeofmine}\n BlockHash is {self.blockid}")
                 self.blockmined = True
+                self.miner = minerAddress
                 self.calculateRaw()
                 break
-                
-        
+
+    # This checks if a block is valid, after its being mined, by hashing the data again with the nonce provided 
     def validateBlock(self):
+
         checkid = str(self.nonce) + str(self.blocktime) + str(self.height) + self.previousblockhash + str(self.difficulty) + self.merkle
         for x in self.transactions:
+            TxLength = str(hex(len(x))[2:]).zfill(8)
+            checkid += TxLength
             checkid += x
-        checkid = hashlib.sha256(checkid.encode('utf-8')).hexdigest()[1:]
+        checkid += self.miner
+
+        checkid = hashlib.sha256(checkid.encode('utf-8')).hexdigest()[2:]
+
         if int(checkid, 16) == int(self.blockid, 16):
+            self.blockmined = True
             print("Validated Block")
         else:
+            self.blockmined = False
             print("False!")
 
+    # This is the function to calculate a raw version of the block after it has been mined.
     def calculateRaw(self):
         if self.blockmined == False:
             print("Cannot generate raw of unmined block")
         else:
+
+            # These are the standard form of a block in raw format
+            # BLOCKID - 64 chars
+            # NONCE - 16 chars
+            # BLOCKTIME - 13 chars
+            # BLOCKHEIGHT - 10 chars
+            # PREVIOUSBLOCKHASH - 64 chars
+            # DIFFICULTY - 96 chars
+            # MERKLE - 96 chars
+            # SIZETX ~ amount of chars in the blocks transactions - 10 chars 
+            # TRANSACTIONS - Variable chars, each transaction is prepended with a length stored in 8 chars
+            # THE MINER ADDRESS - 64 chars
+
             nonce = hex(self.nonce)[2:]
             nonce = nonce.zfill(16)
             blocktime = str(self.blocktime).zfill(13)
@@ -104,7 +120,7 @@ class Block():
                 transactions += x
             sizeTx = hex(len(transactions))[2:]
             sizeTx = sizeTx.zfill(10)
-            raw = self.blockid + nonce + blocktime + blockheight + self.previousblockhash + difficultlyOfBlock + self.merkle + sizeTx + transactions
+            raw = self.blockid + nonce + blocktime + blockheight + self.previousblockhash + difficultlyOfBlock + self.merkle + sizeTx + transactions + self.miner
             self.raw = raw
             print("----------------------")
             print(f"BLOCK ID: {self.blockid}")
@@ -116,8 +132,8 @@ class Block():
             print(f"MERKLE ROOT: {self.merkle}")
             print(f"SIZE OF TXS : {sizeTx}")
             print(f"TXS : {transactions}")
+            print(f"MINER: {self.miner}")
             print("----------------------")
-            self.addBlockToChain()
     
     def addBlockToChain(self):
         if self.blockmined == False:
@@ -131,11 +147,8 @@ class Block():
     
     def createBlockFromRaw(self, raw):
         #Grabbing info from the raw block data
-
         blockid = raw[0:64]
-        print(f"BlockIdFromRaw: {blockid}")
         nonce = raw[64:80]
-        print(f"Nonce: {nonce}")
         blocktime = raw[80:93]
         blockheight = raw[93:103]
         prevblockid = raw[103:167]
@@ -143,12 +156,19 @@ class Block():
         merkle = raw[263:327]
         sizeofTXs = int(raw[327:337], 16)
         transactions = raw[337:337+sizeofTXs]
-        index = 337
+        miner = raw[-66:]
         
-        while True:
-            size = transactions[0:8]
-            index2 += int(size, 16)
+        #Parsing the transactions from raw to a transactions array
+        index = 0
+        self.transactions = []
 
+        while index != len(transactions):
+            txSize = transactions[0:8]
+            txSize = int(txSize, 16)
+            index += 8
+            tx = transactions[index:index+txSize]
+            self.transactions.append(tx)
+            index += txSize
 
         #Correlating the current blocks attributes to these variables
         self.blockid = blockid
@@ -158,25 +178,30 @@ class Block():
         self.previousblockhash = prevblockid
         self.difficulty = int(difficulty, 16)
         self.merkle = merkle
-
-
-        
-
-
+        self.miner = miner
 
 def main():
 
-    x = Block(1, ["0102ab696a951348d80c9360d0de0733eef12c6cd64e7bbaaf658acee42a61d32d600001002046b7ebfe9b9639f6f88f77709b453f89ce380ae192202f1fd913864a4c3144948732d097e715d15e8ddd312749a97572bc97b6f8bc1692f08e82f90d0882258e00010020c2d28ed3a36ca0a8a3076d4c2dfa54c95383deee8ed16b63720f0561a86894650100000000000001f4002676a92169f38a6b51de7e9345992f2161c9c811a8b57cb2c1f31b8f98211b21af61096bd588ac00000000"], 110568172840591261314241097251219333984481879657863353467234327275223423263, "1b7990b9fd11da0d24a0f539e3ed3407285538737785acc6b7dcb602b0a68492")
+    x = Block(1, ["0102ab696a951348d80c9360d0de0733eef12c6cd64e7bbaaf658acee42a61d32d600001002046b7ebfe9b9639f6f88f77709b453f89ce380ae192202f1fd913864a4c3144948732d097e715d15e8ddd312749a97572bc97b6f8bc1692f08e82f90d0882258e00010020c2d28ed3a36ca0a8a3076d4c2dfa54c9538555558ed16b63720f0561a86894650100000000000001f4002676a92169f38a6b51de7e9345992f2161c9c811a8b57cb2c1f31b8f98211b21af61096bd588ac00000000", "0102ab696a951348d80c9360d0de0733eef12c6cd64e7bbaaf658acee42a61d32d600001002046b7ebfe9b9639f6f88f77709b453f89ce380ae192202f1fd913864a4c3144948732d097e715d15e8ddd312749a97572bc97b6f8bc1692f08e82f90d0882258e00010020c2d28ed3a36ca0a8a3076d4c2dfa54c95383deee8ed16b63720f0561a86894650100000000000001f4002676a92169f38a6b51de7e9345992f2161c9c811a8b57cb2c1f31b8f98211b21af61096bd588ac00000001"], 110568172840591261314241097251219333984481879657863353467234327275223423263, "1b7990b9fd11da0d24a0f539e3ed3407285538737785acc6b7dcb602b0a68492")
     # # #y = Block(2, ["0102ab696a951348d80c9360d0de0733eef12c6cd64e7bbaaf658acee42a61d32d600001002046b7ebfe9b9639f6f88f77709b453f89ce380ae192202f1fd913864a4c3144948732d097e715d15e8ddd312749a97572bc97b6f8bc1692f08e82f90d0882258e00010020c2d28ed3a36ca0a8a3076d4c2dfa54c95383deee8ed16b63720f0561a86894650100000000000001f400201b7990b9fd11da0d24a0f539e3ed3407285538737785acc6b7dcb602b0a6849200000000"], 110568172798640591261314241097512146939888796578635346723327275266671322463)
-    x.mine()
+    x.mine("ab696a951348d80c9360d0de0733eef12c6cd64e7bbaaf658acee42a61d32d60")
+    x.validateBlock()
     print(x.raw)
 
     v = Block()
     v.createBlockFromRaw(x.raw)
+    print(v.transactions)
 
+    if x.transactions == v.transactions:
+        print(True)
     
+    print(v.miner)
+    if x.miner == v.miner: 
+        print("Correct Miner")
 
-
+    if v.blockid == x.blockid:
+        print("SOMEHOW?")
+    
     #findTXID("ab696a951348d80c9360d0de0733eef12c6cd64e7bbaaf658acee42a61d32d60", "blockchain.pickle")
 
 if __name__ == "__main__":
--- code/rawtxcreator.py
+++ code/rawtxcreator.py
@@ -1,4 +1,5 @@
 import hashlib
+from spubKeycreator import createPayToPubKeyHash
 
 def createTxFromUser():
 
@@ -84,29 +85,52 @@ def createTxFromDict(tx2):
    return rawtx
 
 def createTXID(rawtx):
-   rawtx1 = hashlib.sha256(rawtx.encode('utf-8')).hexdigest()
-   txid = hashlib.sha256(rawtx1.encode('utf-8')).hexdigest()
+   txid = hashlib.sha256(rawtx.encode('utf-8')).hexdigest()
    return txid
+
+def createCoinbaseTx(publicKey, reward):
+   # The coinbase transaction is the reward the u
+   CoinbaseTemplateDict = {
+      "Version":"01",
+      "InputCount":"01",
+      "txid0":"8266deca6c65b39468e6fb8596869a231b9582ee3818d12ba7240cb126ebfb44", #hash of 'COINBASE'
+      "vout0":"0001",
+      "sizeSig0":"000A",
+      "scriptSig0":"0000000000",
+      "OutputCount":"01",
+      "value0":"",
+      "sizePk0":"",
+      "scriptPubKey0":"",
+      "locktime":"00000000"   
+   }
+
+   scriptPubKey = createPayToPubKeyHash(publicKey)
+   value = str(hex(reward)[2:]).zfill(16)
+   CoinbaseTemplateDict["value0"] = value
+   
+
+   pass
 
 def main():
 
-   print("tx from dict")
-   dict = {
-   "Version":"01",
-   "InputCount":"01",
-   "txid0":"ab696a951348d80c9360d0de0733eef12c6cd64e7bbaaf658acee42a61d32d60",
-   "vout0":"0001",
-   "sizeSig0":"0020",
-   "scriptSig0":"46b7ebfe9b9639f6f88f77709b453f89ce380ae192202f1fd913864a4c314494",
-   "OutputCount":"01",
-   "value0":"00000000000001f4",
-   "sizePk0":"0026",
-   "scriptPubKey0":"76a92169f38a6b51de7e9345992f2161c9c811a8b57cb2c1f31b8f98211b21af61096bd588ac",
-   "locktime":"00000000"   
-   }
+   # print("tx from dict")
+   # dict = {
+   # "Version":"01",
+   # "InputCount":"01",
+   # "txid0":"ab696a951348d80c9360d0de0733eef12c6cd64e7bbaaf658acee42a61d32d60",
+   # "vout0":"0001",
+   # "sizeSig0":"0020",
+   # "scriptSig0":"46b7ebfe9b9639f6f88f77709b453f89ce380ae192202f1fd913864a4c314494",
+   # "OutputCount":"01",
+   # "value0":"00000000000001f4",
+   # "sizePk0":"0026",
+   # "scriptPubKey0":"76a92169f38a6b51de7e9345992f2161c9c811a8b57cb2c1f31b8f98211b21af61096bd588ac",
+   # "locktime":"00000000"   
+   # }
 
-   rawtx = createTxFromDict(dict)
-   print(rawtx)
+   # rawtx = createTxFromDict(dict)
+   # print(rawtx)
+   createCoinbaseTx("dasd", 100)
 
 
 
